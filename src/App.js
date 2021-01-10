0cb333729f1bfb058477a0d6191fbf042877376a
You Song Hou <yousonghou@yahoo.com>
2021-01-09 22:53:32 -0500

    install bridge-sdk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { Component} from 'react';
+import { BridgeSDK } from 'bridge-sdk';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Fund Restaurant</h1>
-    </div>
-  );
+class App extends Component {
+  async componentDidMount(){
+    const configs = require('bridge-sdk/lib/configs');
+    const bridgeSDK = new BridgeSDK({ logLevel: 0 });
+
+    await bridgeSDK.init(configs.testnet);
+
+    bridgeSDK.setUseMetamask(true);
+    bridgeSDK.setUseOneWallet(true);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Fund Restaurant</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
