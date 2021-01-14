9c37509e2afb216fceb5b545f876e244d13c9bb5
You Song Hou <yousonghou@yahoo.com>
2021-01-14 00:19:53 -0500

    add bootstrap styles on distribution tokens form

--- src/components/DistributeTokens.js
+++ src/components/DistributeTokens.js
@@ -45,46 +45,65 @@ function DistributeTokens({ sendTokensToEth }) {
   }
 
   return (
-    <div>
-      <h2>Distribute Tokens</h2>
+    <div className="container">
+      <h2 className="text-center my-2">Distribute Tokens</h2>
 
-      <h5>Your One Wallet Address:</h5>
-      {!oneaddress
-        ? <button onClick={() => getOneWalletAddress()}>Connect With One Wallet</button>
-        : <p>{oneaddress}</p>
-      }
-      <br />
-      <br />
-      <br />
-
-      {addressList.map(address => {
-        return (
-          <div key={address.address}>
-            <p>{address.address} - {address.receiveToken ? "Yes" : "No"}</p>
+      <div className="row">
+        <div className="col-12 col-md-5">
+          <div className="card">
+            <div className="card-body">
+              <h5>One Wallet Address:</h5>
+              {!oneaddress
+                ? <button className="btn secondary-color" onClick={() => getOneWalletAddress()}>Connect With One Wallet</button>
+                : <p>{oneaddress}</p>
+              }
+            </div>  
           </div>
-          
-        )
-      })}
 
-      <input
-        type="text"
-        placeholder="Eth Address"
-        value={ethaddress}
-        onChange={e => setEthaddress(e.target.value)} />
-      <button onClick={() => addAddress()}>
-        Add Address
-      </button>
-      <br />
-      <br />
-      <input
-        type="number"
-        placeholder="Amount"
-        value={amount}
-        onChange={e => setAmount(e.target.value)} />
-      <br />
-      <button onClick={() => distributeTokens()} disabled={loading}>
-        {loading ? "Pending" : "Distribute Tokens"}
-      </button>
+          <div className="card mt-3">
+            <div className="card-body">
+              <p>Total Addresses: 0</p>
+
+              <div className="d-flex align-items-center">
+                <p className="mt-1">Amount: </p>
+                <input
+                  type="number"
+                  placeholder="Amount"
+                  value={amount}
+                  onChange={e => setAmount(e.target.value)} />
+              </div>
+
+              <p>Token Type: BUSD</p>
+              <button className="btn primary-color btn-block" onClick={() => distributeTokens()} disabled={loading}>
+                {loading ? "Pending" : "Distribute Tokens"}
+              </button>
+            </div>  
+          </div>
+        </div>
+
+        <div className="col-12 col-md-7">
+          <div className="card">
+            <div className="card-body">
+              <h2>List of Addresses</h2>
+              {addressList.map(address => {
+                return (
+                  <div key={address.address}>
+                    <p>{address.address} - {address.receiveToken ? "Yes" : "No"}</p>
+                  </div>
+                )
+              })}
+              <input
+                type="text"
+                placeholder="Eth Address"
+                value={ethaddress}
+                onChange={e => setEthaddress(e.target.value)} />
+              <button className="btn primary-color" onClick={() => addAddress()}>
+                Add Address
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>  
     </div>
   );
 }
