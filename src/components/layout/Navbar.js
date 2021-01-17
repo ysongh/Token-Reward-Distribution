5569dc73d26d9afc55a2f818fbf566350f1f60e8
You Song Hou <yousonghou@yahoo.com>
2021-01-16 22:28:39 -0500

    change background color

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function Navbar(){
   return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light third-color borderline-primary">
       <div className="container">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand text-secondary-color" to="/">
           Token Reward Distribution
         </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,13 +14,13 @@ function Navbar(){
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link text-primary-color" to="/">Home</Link>
               </li>
               <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                <Link className="nav-link" to="/transfer">Transfer Token</Link>
+                <Link className="nav-link text-primary-color" to="/transfer">Transfer Token</Link>
               </li>
               <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                <Link className="nav-link" to="/distribute">Distribute Token</Link>
+                <Link className="nav-link text-primary-color" to="/distribute">Distribute Token</Link>
               </li>
             </ul>
         </div>
